253d4ae46193b76abc35c2c9bea5fd9f68e575b8
kranthi kumar <kranthi.8t7@gmail.com>
2019-05-22 00:33:36 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,7 @@
-var lyric = "maybe";
 
 function callMe() {
+  var lyric = "maybe";
+
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -17,15 +18,17 @@ function crazy() {
     console.log("hey!!!")
   }
 }
-var name = "Cricky";
-var name = "Kristin";
 
 function sayMyName() {
+  var name = "Cricky";
+
   // fix the code in here:
 
   sayMy();
 
   function sayMy() {
+    var name = "Kristin";
+
     console.log(name);
   }
 }
